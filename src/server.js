25b1e69591f19c667aa688b3411bf4e3c6242f5f
GuyNa <34960387+guyna25@users.noreply.github.com>
2023-03-17 22:44:54 +0200

    Restructured files and added some db functionality

--- src/server.js
+++ src/server.js
@@ -2,24 +2,22 @@
 const http = require('http');
 const path = require('path');
 const mongoose = require('mongoose');
+require('dotenv').config();
 //internal imports
-const {get_products} = require('./product_scraper');
-const {get_product_grid_html} = require('./product_display');
-const {get_full_access_url} = require('./db_password_loader');
+const {get_products} = require('./services/product_scraper');
+const {get_product_grid_html} = require('./views/product_display');
 const {createProducts, getAllProducts} = require('./models/products.model');
-
+const {mongoConnect} = require('./services/db');
 
 //constants
 const PORT = 3000;
 const serach_words = ['ducks'];
-const mongo_pass_path = 'product_viwer_pass.json';
-const MONGO_URL = get_full_access_url(mongo_pass_path);
 
 //variables
 const server = http.createServer();
 
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    mongoConnect();
     get_products(serach_words).then((vals) => {
         let product_arr = vals.filter((p) => {
             return !(p.name == '' && p.price == '' && p.rating == '' && p.image == '');
@@ -34,14 +32,6 @@ async function startServer() {
 };
 
 startServer();
-
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
   
 server.on('request', (req, res)=> {
     const items = req.url.split('/');
