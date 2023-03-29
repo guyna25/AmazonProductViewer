f9115507d2a299d0011831d6d9deaba685e3ee82
GuyNa <34960387+guyna25@users.noreply.github.com>
2023-03-29 18:50:00 +0300

    Added search bar to main page

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,73 +1,49 @@
-import 'dart:html';
-
+import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
-String _msg = "";
-
-void _testServer() {
-  get("http://localhost:3000/products_test").then((res) {
-    _msg = "Success";
-    print(res.body);
-  });
-}
 
 void main() {
-  _testServer();
-  runApp(const MyApp());
+  runApp(const AppBase());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppBase extends StatefulWidget {
+  const AppBase({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<AppBase> createState() => _AppBaseState();
+}
+
+class _AppBaseState extends State<AppBase> {
+  String searchValue = '';
+  final List<String> _suggestions = [
+    'Ducks',
+    'Lotion',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Amazon product comparison',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Amazon product comparison'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Test',
-            ),
-            Text(
-              _msg,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _testServer,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        title: 'Price Comparison',
+        theme: ThemeData(primarySwatch: Colors.orange),
+        home: Scaffold(
+            appBar: EasySearchBar(
+                title: const Text('Enter product name'),
+                onSearch: (value) => setState(() => searchValue = value),
+                suggestions: _suggestions),
+            drawer: Drawer(
+                child: ListView(padding: EdgeInsets.zero, children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () => Navigator.pop(context)),
+              ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () => Navigator.pop(context))
+            ])),
+            body: Center(child: Text('Value: $searchValue'))));
   }
 }
